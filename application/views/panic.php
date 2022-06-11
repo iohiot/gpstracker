47866be5315e81325b6371c92fd031a3c780d2b8
Redi Herdian Kustiwa <redi.xapiens.id>
2022-06-11 17:03:41 +0700

    Init App

---
+++ application/views/panic.php
@@ -0,0 +1,50 @@
+<div class="content-header">
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-sm-6">
+                <h1 class="m-0 text-dark">Panic
+                </h1>
+            </div><!-- /.col -->
+            <div class="col-sm-6">
+                <ol class="breadcrumb float-sm-right">
+                    <li class="breadcrumb-item"><a href="<?= base_url(); ?>/vehicle">Vehicle</a></li>
+                    <li class="breadcrumb-item active">Panic</li>
+                </ol>
+            </div><!-- /.col -->
+        </div><!-- /.row -->
+    </div><!-- /.container-fluid -->
+</div>
+<!-- Main content -->
+<section class="content">
+    <div class="container-fluid">
+        <form method="post" id="form-panic" class="card">
+            <input type="hidden" value="0" id="panicinput" name="panicinput">
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="col-sm-3">&nbsp;</div>
+                    <div class="col-sm-12">
+                        <button type="button" class="btn btn-block btn-primary" id="btn-new">New Location</button>
+                    </div>
+                    <div class="col-sm-3">&nbsp;</div>
+                    <div class="col-sm-12">
+                        <button type="button" id="btn-panic" class="btn btn-block btn-danger">Panic</button>
+                    </div>
+                    <div class="col-sm-3">&nbsp;</div>
+                </div>
+            </div>
+        </form>
+    </div>
+</section>
+<!-- /.content -->
+
+<script>
+    $("#btn-new").on("click", function(event) {
+        $("#panicinput").val(0)
+        $("#form-panic").submit()
+    });
+
+    $("#btn-panic").on("click", function(event) {
+        $("#panicinput").val(1)
+        $("#form-panic").submit()
+    });
+</script>
---
+++ assets/livetrack.js
@@ -0,0 +1,147 @@
+var map = (infoWindow = latitude = null);
+var markersArray = [];
+window.onload = function () {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      document.cookie = "latitude=" + position.coords.latitude;
+      document.cookie = "longitude=" + position.coords.longitude;
+    });
+  }
+  map = new google.maps.Map(document.getElementById("map_canvas"), {
+    center: new google.maps.LatLng(-6.2, 106.816666),
+    zoom: 5,
+    mapTypeId: "roadmap",
+    gestureHandling: "greedy",
+  });
+  infoWindow = new google.maps.InfoWindow();
+  livetracking();
+  window.setInterval(livetracking, 15000);
+};
+
+function livetracking() {
+  if ($("#group").attr("data-name") != 0) {
+    var path =
+      $("#base").val() +
+      "/api/currentpositions?gr=" +
+      $("#group").attr("data-name");
+  } else {
+    var path = $("#base").val() + "/api/currentpositions";
+  }
+
+  $.ajax({
+    type: "GET",
+    url: path,
+    dataType: "json",
+    cache: false,
+    success: function (result) {
+      if (result.status == 1) {
+        var j;
+        var markers = result.data;
+        var bounds = new google.maps.LatLngBounds();
+        resetMarkers(markersArray);
+        for (i = 0; i < markers.length; i++) {
+          var lastupdate = markers[i].time;
+
+          if (markers[i].v_type == "MOTORCYCLE") {
+            var v_type = fontawesome.markers.MOTORCYCLE;
+          } else if (markers[i].v_type == "BICYCLE") {
+            var v_type = fontawesome.markers.BICYCLE;
+          } else if (markers[i].v_type == "CAR") {
+            var v_type = fontawesome.markers.CAR;
+          } else if (markers[i].v_type == "TRUCK") {
+            var v_type = fontawesome.markers.TRUCK;
+          } else if (markers[i].v_type == "BUS") {
+            var v_type = fontawesome.markers.BUS;
+          } else if (markers[i].v_type == "TAXI") {
+            var v_type = fontawesome.markers.TAXI;
+          } else {
+            var v_type = fontawesome.markers.TRUCK;
+          }
+
+          baseUrl = "http://localhost/vms/assets/marker/";
+          v_type = baseUrl + "marker-gold.png";
+          if (markers[i].is_panic == 1) {
+            v_type = baseUrl + "loading.gif";
+          }
+          var point = new google.maps.LatLng(
+            parseFloat(markers[i].latitude),
+            parseFloat(markers[i].longitude)
+          );
+          var html =
+            "<div class=' '><b>" +
+            "Name: </b>" +
+            markers[i].v_name +
+            "<br>" +
+            "<b>Speed: </b>" +
+            Math.round(markers[i].speed) +
+            " Km/h<br>" +
+            "<b>Updated On: </b>" +
+            lastupdate +
+            "<br></div>";
+          var marker = new google.maps.Marker({
+            map: map,
+            position: point,
+            icon: v_type,
+            // icon: {
+            //   url: v_type,
+            //   size: new google.maps.Size(30, 30),
+            //   scaledSize: new google.maps.Size(30, 30),
+            // anchor: new google.maps.Point(0, 0),
+            // strokeWeight: 0.2,
+            // strokeColor: "black",
+            // strokeOpacity: 2,
+            // fillColor: markers[i].v_color,
+            // fillOpacity: 1.5,
+            // },
+            //BICYCLE,CAR,MOTORCYCLE,TRUCK
+            //shadow : icon
+          });
+          markersArray.push(marker);
+          bindInfoWindow(marker, map, infoWindow, html);
+        }
+      } else {
+        alertmessage(result.message, 2);
+      }
+    },
+    error: function (jqXHR, textStatus, errorThrown) {
+      console.log("Unexpected error.");
+    },
+  });
+}
+function resetMarkers(arr) {
+  for (var i = 0; i < arr.length; i++) {
+    arr[i].setMap(null);
+  }
+  arr = [];
+}
+function bindInfoWindow(marker, map, infoWindow, html) {
+  google.maps.event.addListener(marker, "click", function () {
+    infoWindow.setContent(html);
+    infoWindow.open(map, marker);
+  });
+}
+function alertmessage(msg, type) {
+  if (type == 1) {
+    $.toast({
+      heading: "Success",
+      text: msg,
+      icon: "info",
+      loader: true,
+      position: "top-center",
+      loaderBg: "#2196f3",
+      afterHidden: function () {
+        location.reload();
+      },
+    });
+  }
+  if (type == 2) {
+    $.toast({
+      heading: "Error",
+      text: msg,
+      icon: "error",
+      loader: true,
+      position: "top-center",
+      loaderBg: "#f44336",
+    });
+  }
+}
