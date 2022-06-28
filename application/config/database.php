3541299e05b37df9234b4612046faf3bb0fe88c8
dump <107100398+iohiot@users.noreply.github.com>
2022-06-28 09:40:48 +0700

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -37,7 +37,7 @@
 // The following values will probably need to be changed.
 $db['default']['username'] = "root";
 $db['default']['password'] = "";
-$db['default']['database'] = "gpstracker";
+$db['default']['database'] = "personal_tracker";
 
 // The following values can probably stay the same.
 $db['default']['hostname'] = "localhost";
